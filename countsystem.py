eecfd117f9e03f7d6133fdc12d2a156deacf0f7d
bonnybelle <55929630+bonnybelle@users.noreply.github.com>
2019-10-30 19:01:54 +0200

    Update countsystem.py

--- countsystem.py
+++ countsystem.py
@@ -1,11 +1,11 @@
-def co(x):
+def co(x, y):
     lst = []
     while x > 0:
-        x1 = x % 2
-        x = x // 2
+        x1 = x % y
+        x = x // y
         lst.insert(0, str(x1))
     return ''.join(lst)
 
 
-print(co(x=int(input('Enter the number: '))))
-
+print(co(x=int(input('Enter the number: ')),
+         y=int(input('Enter the system: '))))
